d5cc5a0a3d08f7582a2816260ce6b4d7335a4d0c
Norberto Ignacio <nico.ignaciojr@mclinica.com>
2021-12-04 22:13:44 +0800

    initial setup

---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,12 @@
+const theme = {
+    primary: {
+        light: "#76FF03",
+        main: "#64DD17",
+        dark: "#8BC34A",
+    },
+    secondary: {
+        main: "#B2FF59",
+    },
+};
+
+export default theme;
