1f38043b5693a4cf05e6e5552c9425b71aaa04d6
Norberto Ignacio <nico.ignaciojr@mclinica.com>
2021-12-16 12:48:01 +0800

    added 404 page

--- src/components/pages/NotFoundPage/NotFoundPage.tsx
+++ src/components/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 
 import PageWrapper from "components/shared/PageWrapper";
-import PetList from "components/PetList";
+import {
+    NotFoundMain,
+    NotFoundPhoto,
+    NotFoundTitle,
+    NotFoundDesc,
+} from "./styles";
+import notFoundPic from "assets/img/notfound.png";
 
 interface IProps {}
 
 const NotFoundPage: React.FC<IProps> = () => {
-    return <PageWrapper title="404 PAGE">NOT FOUND</PageWrapper>;
+    return (
+        <PageWrapper title="">
+            <NotFoundMain>
+                <NotFoundPhoto src={notFoundPic} alt="404 Page Photo" />
+                <NotFoundTitle>Oops!</NotFoundTitle>
+                <NotFoundDesc>
+                    Looks like FindMEow had an error.{" "}
+                    <span>Try reloading the page!</span>
+                </NotFoundDesc>
+            </NotFoundMain>
+        </PageWrapper>
+    );
 };
 
 export default NotFoundPage;
--- src/components/pages/NotFoundPage/styles.tsx
+++ src/components/pages/NotFoundPage/styles.tsx
@@ -1,1 +1,32 @@
 import styled from "styled-components";
+
+export const NotFoundMain = styled.div`
+    width: 80%;
+    margin: 0 auto;
+    display: grid;
+    gap: 2rem;
+    justify-items: center;
+    align-content: center;
+    align-items: center;
+    text-align: center;
+    padding-top: 2rem;
+`;
+
+export const NotFoundPhoto = styled.img`
+    display: block;
+    width: 400px;
+    height: auto;
+    object-fit: cover;
+`;
+
+export const NotFoundTitle = styled.h3`
+    font-size: 3rem;
+`;
+
+export const NotFoundDesc = styled.p`
+    font-size: 2rem;
+    font-weight: 300;
+    span {
+        display: block;
+    }
+`;
